6ebeb539042868591138bfbb91a6f49a44be4c79
Gotlop <haitayo750@gmail.com>
2025-04-12 20:58:52 +0700

    Update route.ts

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,13 +4,13 @@ export async function GET() {
   const appUrl = process.env.NEXT_PUBLIC_URL || `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
   const config = {
-  {
-  "accountAssociation": {
-    "header": "eyJmaWQiOjU0MDExOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDRjNjcwZDg1ZGQ2MzJlZmQwODVGODdjYWE1NEQ0Q2M5MTExZTkzYjgifQ",
-    "payload": "eyJkb21haW4iOiJ0ZXN0ZnJhbWVkLTczZHMudmVyY2VsLmFwcCJ9",
-    "signature": "MHgzNTE5NmQ1NGUzMThkYTczYzQ4NDIxMTg0YmE3ZGRkMGYxOGEzNGViNzg3NzI4ZTI4ZmJiNGU5OGYxZWM3ZTk3NzA0Y2UzOGFlZDU1MzA3NGM5NDAyNzkzODYzZmI5YTk4NGQzNzgxNmRkYzQ5M2VlOThiNzI3M2ZhOGYxYzk1MDFi"
-  }
-}
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjU0MDExOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDRjNjcwZDg1ZGQ2MzJlZmQwODVGODdjYWE1NEQ0Q2M5MTExZTkzYjgifQ",
+      payload: "eyJkb21haW4iOiJ0ZXN0ZnJhbWVkLTczZHMudmVyY2VsLmFwcCJ9",
+      signature:
+        "MHgzNTE5NmQ1NGUzMThkYTczYzQ4NDIxMTg0YmE3ZGRkMGYxOGEzNGViNzg3NzI4ZTI4ZmJiNGU5OGYxZWM3ZTk3NzA0Y2UzOGFlZDU1MzA3NGM5NDAyNzkzODYzZmI5YTk4NGQzNzgxNmRkYzQ5M2VlOThiNzI3M2ZhOGYxYzk1MDFi",
+    },
     frame: {
       version: "1",
       name: PROJECT_TITLE,
